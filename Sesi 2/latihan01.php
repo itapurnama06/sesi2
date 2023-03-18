b2c2fbd6e4ebff6a2022f09c513185124dbda90e
itapurnama06 <127809157+itapurnama06@users.noreply.github.com>
2023-03-18 11:20:59 +0700

    upload sesi 2

---
+++ Sesi 2/latihan01.php
@@ -0,0 +1,7 @@
+<?php
+   echo"Belajar Script PHP";
+   $dta = "Ita";
+   $angka =5;
+   echo '<br>Nama '.$dta." Peringkat Ke-".$angka ;
+   echo '<br>Nama $dta'. "Anak ke-".$angka;
+   echo "<br>Nama: $dta anak ke-$angka";
---
+++ Sesi 2/latihan04.php
@@ -0,0 +1,16 @@
+<?php
+  $nVar = "Tiga";
+  switch($nVar){
+      case "satu":
+        echo "nVar bernilai Satu";
+        break;
+      case "dua":
+        echo "nVar bernilai dua";
+        break;
+      case "tiga":
+        echo "nVar bernilai tiga";
+        break;
+    default:
+        echo "nVar bernilai acak";
+        break;
+      }
